b68aedafddb42ab92e634bf2f8f9e282510fede6
Hashim Warren <hashimwarren@gmail.com>
2018-07-03 21:05:15 -0400

    Make while loop example

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-<h1><?php bloginfo('name'); ?> </h1>
-<h2><?php bloginfo('description'); ?><h2>
+<?php
+
+$names = array("Hashim", "Alex", "Madison", "Moses");
+
+$count = 1;
+while ($count < 100) {
+    echo "<li>$count</li>";
+    $count++;
+}
+?>
+
+<p>Hi, my name is <?php echo $names[0]; ?> and I like candy</p>
+<p>Hi, my name is <?php echo $names[1]; ?> and I like candy</p>
+<p>Hi, my name is <?php echo $names[2]; ?> and I like candy</p>
+<p>Hi, my name is <?php echo $names[3]; ?> and I like candy</p>

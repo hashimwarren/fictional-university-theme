b7921d40dba8f4f1a9a603136bde4ddcd93ba373
Hashim Warren <hashimwarren@gmail.com>
2018-07-06 16:28:43 -0400

    Create basic template for homepage

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php
 
-$names = array("Hashim", "Alex", "Madison", "Moses", "Marilyn");
+while(have_posts()){
+    the_post(); ?>
+    <h2><a href="<?php the_permalink() ?>"> <?php the_title() ?></a></h2>
+    <?php the_content() ?>
+    <hr>
+<?php }
 
-$count = 0;
 
-while ($count < count($names)) {
-    echo "<li>Hi my name is $names[$count]</li>";
-    $count++;
-}
 
 ?>
 
-<p>Hi, my name is <?php echo $names[0]; ?> and I like candy</p>
-<p>Hi, my name is <?php echo $names[1]; ?> and I like candy</p>
-<p>Hi, my name is <?php echo $names[2]; ?> and I like candy</p>
-<p>Hi, my name is <?php echo $names[3]; ?> and I like candy</p>
